bd1734e691f02c54386918ec195428e9032795ce
HongBeen <redbean-2@naver.com>
2022-04-15 20:04:09 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
-console.log("hello world");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-app.listen(3000);
+// TODO: use는 왜쓰는거지..
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = ()=>{
+  console.log("success port 3000")
+}
+app.listen(3000,handleListen);
